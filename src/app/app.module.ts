675a043e5c6d32da5584e38ced6fb7682264f00d
TonyGregg <antonygenil@gmail.com>
2019-12-15 20:19:10 -0600

    Added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { TacoService } from './shared/taco.service';
 import { HighlightDirective } from './common/highlight.directive';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
--- src/app/common/highlight.directive.ts
+++ src/app/common/highlight.directive.ts
@@ -2,7 +2,7 @@ import { Directive, ElementRef } from '@angular/core';
 /**
  * It is an attribute directive - changes the appearance or behavior of a DOM element
  * There are three kinds of directives in Angular:
-* 1. Components—directives with a template.
+*  1. Components—directives with a template.
   2. Structural directives—change the DOM layout by adding and removing DOM elements, e.g. *ngFor or *ngIf
   3. Attribute directives—change the appearance or behavior of an element, component, or another directive.
  */
@@ -12,7 +12,7 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class HighlightDirective {
 
-  constructor(el: ElementRef) { 
+  constructor(el: ElementRef) {
     el.nativeElement.style.backgroundColor = 'yellow';
   }
 
